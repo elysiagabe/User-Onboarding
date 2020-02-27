6993893f1047275bb80a55c3d04393c11607f9c2
Elysia Gabe <elysiagabe@gmail.com>
2020-02-27 14:59:56 -0800

    Implements form validation & error messaging

--- user-onboarding/src/Components/OnboardingForm.js
+++ user-onboarding/src/Components/OnboardingForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-const OnboardingForm = ({values}) => {
+const OnboardingForm = ({values, touched, errors}) => {
     return (
         <div>
             <Form>
@@ -12,6 +13,7 @@ const OnboardingForm = ({values}) => {
                         type="text" 
                         name="name" 
                     />
+                    {touched.name && errors.name && <p>{errors.name}</p>}
                 </label>
 
                 <label htmlFor="email">
@@ -21,6 +23,7 @@ const OnboardingForm = ({values}) => {
                         type="email" 
                         name="email" 
                     />
+                    {touched.email && errors.email && <p>{errors.email}</p>}
                 </label>
 
                 <label htmlFor="password">
@@ -30,6 +33,7 @@ const OnboardingForm = ({values}) => {
                         type="password"
                         name="password"
                     />
+                    {touched.password && errors.password && <p>{errors.password}</p>}
                 </label>
 
                 <label htmlFor="acceptTerms">
@@ -40,6 +44,7 @@ const OnboardingForm = ({values}) => {
                         checked={values.acceptTerms}
                     />
                     I have read through and agree to the terms of service.
+                    {touched.acceptTerms && errors.acceptTerms && <p>{errors.acceptTerms}</p>}
                 </label>
 
                 <button type="submit">Submit ></button>
@@ -56,7 +61,31 @@ const FormikOnboardingForm = withFormik({
             password: password || "",
             acceptTerms: acceptTerms || false,
         }
-    }
+    },
+    // ==== begin validation schema ==== //
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .min(3, "Try again! Your name must be at least 3 characters.")
+            .required("Enter your name"),
+        email: Yup.string()
+            .email("Try again! A valid email address must contain a single @ symbol.")
+            .required("Enter your email address"),
+        password: Yup.string()
+            .min(8, "Try again! Your password must contain at least 8 characters.")
+            .required("Enter a secure password"),
+        acceptTerms: Yup.boolean()
+            .oneOf([true], "Please agree to our terms of service")
+    })
+
+    // validationSchema: Yup.object().shape({
+    //     email: Yup.string()
+    //       .email("Email not valid")
+    //       .required("Email is required"),
+    //     password: Yup.string()
+    //       .min(6, "Password must be 6 characters or longer")
+    //       .required("Password is required")
+    //   }),
+    // ==== end validation schema ==== //
 })(OnboardingForm);
 
 export default FormikOnboardingForm;
