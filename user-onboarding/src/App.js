22b9d2b2641adba5615e9d607a1e817cefd008e2
Elysia Gabe <elysiagabe@gmail.com>
2020-02-27 13:02:04 -0800

    Creates Formik form in OnboardingForm.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import OnboardingForm from "./Components/OnboardingForm";
+import FormikOnboardingForm from "./Components/OnboardingForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
-        <OnboardingForm />
+        <FormikOnboardingForm />
       </header>
     </div>
   );
--- user-onboarding/src/Components/OnboardingForm.js
+++ user-onboarding/src/Components/OnboardingForm.js
@@ -1,10 +1,62 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
 
-const OnboardingForm = () => {
+const OnboardingForm = ({values}) => {
     return (
-        <h2>Hi there. I'm the form component.</h2>
+        <div>
+            <Form>
+                <label htmlFor="name">
+                    Name: 
+                    <Field 
+                        id="name" 
+                        type="text" 
+                        name="name" 
+                    />
+                </label>
+
+                <label htmlFor="email">
+                    Email: 
+                    <Field 
+                        id="email" 
+                        type="email" 
+                        name="email" 
+                    />
+                </label>
+
+                <label htmlFor="password">
+                    Password: 
+                    <Field 
+                        id="password"
+                        type="password"
+                        name="password"
+                    />
+                </label>
+
+                <label htmlFor="acceptTerms">
+                    <Field 
+                        id="acceptTerms"
+                        type="checkbox"
+                        name="acceptTerms"
+                        checked={values.acceptTerms}
+                    />
+                    I have read through and agree to the terms of service.
+                </label>
+
+                <button type="submit">Submit ></button>
+            </Form>
+        </div>
     )
 }
 
-export default OnboardingForm;
+const FormikOnboardingForm = withFormik({
+    mapPropsToValues({name, email, password, acceptTerms}) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            acceptTerms: acceptTerms || false,
+        }
+    }
+})(OnboardingForm);
+
+export default FormikOnboardingForm;
