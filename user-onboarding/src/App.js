02eae4150ca91a8447d5c378fa2e0fb9883a31fc
Elysia Gabe <elysiagabe@gmail.com>
2020-02-27 12:32:14 -0800

    Creates react app, installs necessary dependencies & sets up basic app structure

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import OnboardingForm from "./Components/OnboardingForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello</h1>
+        <OnboardingForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Components/OnboardingForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+
+const OnboardingForm = () => {
+    return (
+        <h2>Hi there. I'm the form component.</h2>
+    )
+}
+
+export default OnboardingForm;
